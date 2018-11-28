e5ff641ad8426cb20d11657e2846cbe0fdbfff97
bluesDD <dd11blues@gmail.com>
2018-11-28 23:17:45 +0900

    chapter1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # portfolio_vue.js
 Personal portfolio by vue.js
+
+# install vue.js
+$ npm install vue@csp
--- chapter-1/main.js
+++ chapter-1/main.js
@@ -1,6 +1,6 @@
 const app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello vue.js!'
+        show: true
     }
 })
