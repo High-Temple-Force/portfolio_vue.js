9577c237275c4f84bf5d377d4ff88404bf55faab
bluesDD <dd11blues@gmail.com>
2018-11-29 21:36:59 +0900

    Just finished ch.4

---
+++ chapter-4/main.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: '#app',
+    data: {
+        budget:300,
+        limit: 2,
+        list: [
+            { id: 1, name: 'りんご', price:100 },
+            { id: 2, name: 'ばなな', price:200 },
+            { id: 3, name: 'いちご', price:400 },
+            { id: 4, name: 'おれんじ', price:300 },
+            { id: 5, name: 'めろん', price:500 },
+        ]
+    },
+    computed: {
+        matched: function() {
+            return this.list.filter(function(el) {
+                return el.price <= this.budget
+            }, this)
+        },
+        limited: function() {
+            return this.matched.slice(0, this.limit)
+        }
+    }
+})
